e370ba4976e89d6e0cf5b306579e8079b68dc952
JTrue <truehitt@gmail.com>
2023-04-26 14:33:03 -0400

    added catch all route and debugged.

--- Develop/app.js
+++ Develop/app.js
@@ -1,23 +1,19 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
 
-const app = express()
+const app = express();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-const htmlRouter = require('./routes/html')
-const apiRouter = require('./routes/api')
+const htmlRouter = require("./routes/html");
+const apiRouter = require("./routes/api");
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false}))
-app.use('/', htmlRouter);
-app.use('/api', apiRouter)
-app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use("/api", apiRouter);
+app.use(htmlRouter);
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
+app.use(express.static("public"));
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}.`)
-})
+  console.log(`App listening on port ${port}.`);
+});
--- Develop/routes/html.js
+++ Develop/routes/html.js
@@ -1,19 +1,20 @@
-const express = require('express')
-const router = express.Router()
-const path = require('path')
+const express = require("express");
+const router = express.Router();
+const path = require("path");
 
 // setting up base middleware
 router.use((req, res, next) => {
-    next()
-})
+  next();
+});
 
-// main page
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public', 'index.html'))
-})
 // /notes page
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public', 'notes.html'))
-})
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public", "notes.html"));
+});
 
-module.exports = router
+// catch all route
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
+});
+
+module.exports = router;
